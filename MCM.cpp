4d1eb7e9807ebe71c32a5e024b04a52df6e8750a
Nit <nitishagrawal2003@gmail.com>
2023-02-20 22:37:16 +0530

    TEMP1

--- MCM.cpp
+++ MCM.cpp
@@ -6,24 +6,6 @@ void matrix_chain_mul(int arr[], int n)
     int m[n][n] = {0};
     int s[n][n] = {0};
     int j, min, q;
-    for (int d = 1; d < n - 1; d++)
-    {
-        for (int i = 1; i < n - d; i++)
-        {
-            j = i + d;
-            min = INT_MAX;
-            for (int k = i; k <= j - 1; k++)
-            {
-                q = m[i][k] + m[k + 1][j] + arr[i - 1] * arr[k] * arr[j];
-                if (q < min)
-                {
-                    min = q;
-                    s[i][j] = k;
-                }
-            }
-            m[i][j] = min;
-        }
-    }
 
     for (int d = 1; d < n; d++)
     {
